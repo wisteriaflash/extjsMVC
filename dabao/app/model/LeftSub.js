8dfcbfc1bdeafb14e0276c54c9b7435c6af6f861
wisteriaflash <wisteriaflash@163.com>
2013-04-26 09:27:02 +0800

    merge

--- dabao/app/model/LeftSub.js
+++
@@ -1,9 +0,0 @@
-Ext.define('app.model.LeftSub',{
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'string'},
-        {name: 'text', type: 'string', mapping: 'title'},
-        {name: 'url', type: 'string'}
-    ],
-    belongsTo: 'app.model.Left'
-})
--- dabao/app/model/User.js
+++
@@ -1,8 +0,0 @@
-Ext.define('app.model.User',{
-    extend: 'Ext.data.Model',
-    fields: [
-        {name:'id', type:'number'},
-        {name:'name', type:'string'},
-        {name:'email', type:'string'}
-    ]
-})
